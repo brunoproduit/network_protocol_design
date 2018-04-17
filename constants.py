56eed044e284380f177dec0a30bcfb4a630d462c
Pavel Chikul <pavel.chikul@reglabs.eu>
2018-04-17 15:54:47 +0300

    Constants update.

--- constants.py
+++ constants.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-# General ------------------------------------------------------------------------------------------
-NAME = 'Network protocol design chat '                                          # Name of program
-AUTHOR = 'Sander Mikelsaar, Sasan Reyaeifars, Florian Reiter, Bruno Produit'    # Author of program
-VERSION = '0.1'                                                                 # Version of program
+# General -----------------------------------------------------------------------------------------------------------
+NAME = 'Network protocol design chat '                                                        # Name of program
+AUTHOR = 'Sander Mikelsaar, Sasan Reyaeifars, Florian Reiter, Bruno Produit, Pavel Chikul'    # Author of program
+VERSION = '0.1'                                                                               # Version of program
 
 # UDP -----------------------------------------------------------------------------
 PORT = 7777                             # Default UDP port, can be changed in arg
