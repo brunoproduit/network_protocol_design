ea3f0fd6144a3f6912a6eb4c4b87e72f110e5fb8
Florian Reiter <reg1nleifr@none.lan>
2018-04-24 17:50:04 +0300

    Utils.py and settings.py

--- main.py
+++ main.py
@@ -16,23 +16,23 @@ class Listener(multiprocessing.Process):
         multiprocessing.Process.__init__(self)
         self.address = address
         self.port = port
-    
+
     def run(self):
 
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.bind((INET_ADDR, self.port))
-           
+
         # Receive loop
         while True:
-            try: 
+            try:
                 data, addr = s.recvfrom(1024)
                 print('Connected by', addr)
                 print (data)
-               
+
             except :
                 print ('Terminating server ...')
                 break
-                
+
         s.close()
         print ('Closed the server socket')
         print ('Terminating ...')
@@ -44,12 +44,12 @@ class Sender(multiprocessing.Process):
         self.address = address
         self.address = address
         self.port = port
-        
-        
+
+
     def run(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.connect((self.address, self.port))
-        
+
         while True:
             message = b'hello world'
 
@@ -59,7 +59,7 @@ class Sender(multiprocessing.Process):
                 print ('Terminating server ...')
                 break
 
-# Main, Program entry, arg parsing    
+# Main, Program entry, arg parsing
 if __name__ == '__main__':
     parser = ArgumentParser(description=info())
     parser.add_argument('-H','--host', help='Server TCP port, '\
@@ -67,15 +67,14 @@ if __name__ == '__main__':
                         default=INET_ADDR)
     parser.add_argument('-p','--port', type=int, help='Server TCP port, '\
                         'defaults to %d' % PORT, default=PORT)
-    
+
     args = parser.parse_args()
 
     listener = Listener(INET_ADDR, PORT)
     sender = Sender(INET_ADDR, PORT)
-    
+
     listener.start()
     sender.start()
-    
+
     listener.join()
     sender.join()
-    
---
+++ utils.py
@@ -0,0 +1,56 @@
+import os, uuid, json
+from settings import PGPSettings
+
+class Utils:
+    def __init__(self):
+        self.data = []
+
+    # Helper function to read a file returns a binary string
+    # @param:filefilename string
+    # @return: binary string
+    def readFile(self, filefilename):
+         if os.path.exists(filefilename):
+             f = open(filefilename, 'rb')
+             data = f.read()
+             f.close()
+             return data
+         else:
+            return False
+
+    # Function to write a file as binary string
+    # @param:filename string
+    # @param:directory string
+    # @param:data binary string
+    def writeFile(self, filename, directory, data, overwrite = False):
+        if os.path.exists(directory+ "/" + filename) and not overwrite:
+            print('File already exist, choosing random name...')
+            elements = filename.split(".")
+            filename = str(uuid.uuid4())
+            if len(elements) > 1:
+                extension = elements[-1]
+                filename = filename + "." + extension
+
+        f = open(directory + "/" + filename,'wb') #open in binary
+        f.write(data)
+        f.close()
+        print("File was written to " + directory + "/" + filename)
+
+    # dumps the settings into a json
+    def saveSettings(self, settings):
+        self.writeFile(
+            "settings.json",
+            ".",
+            json.dumps(settings.__dict__).encode(),
+            True
+        )
+
+    # def readSettings(self, filename):
+
+
+    # TODO: SendMessage, SaveSettings, SendUsermessage
+
+utils = Utils()
+pgpsettings = PGPSettings("keys/masterkey.pgp", "keys/masterkey.pgp")
+# utils.writeFile("test.txt", "..", b"Test!")
+print(utils.readFile("../test.txt"))
+utils.saveSettings(pgpsettings)
