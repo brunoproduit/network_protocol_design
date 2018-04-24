524bdca7cdf7f2a30d01caf3daf956b91bb16f53
Pavel Chikul <pavel.chikul@reglabs.eu>
2018-04-24 17:45:08 +0300

    MA.

--- layer5.py
+++ layer5.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+from enum import Enum
+
+
+class Layer5Type(Enum):
+    MESSAGE = 0x01,
+    FILE = 0x02
 
 
 class Layer5:
     def __init__(self):
-        print("blah")
+        self.__init__(Layer5Type.MESSAGE)
+
+    def __init__(self, packet_type):
+        self.type = packet_type
+        # self.payload = ...
