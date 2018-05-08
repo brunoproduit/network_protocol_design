8a899afd436e6fa10c0ce9347bd983a8ab3d4d57
Bruno Produit <bruno.produit@protonmail.ch>
2018-05-08 16:03:18 +0300

    Finished crypto layer

--- crypto.py
+++ crypto.py
@@ -1,68 +1,105 @@
 import pgpy, hashlib
-from pgpy.constants import PubKeyAlgorithm, KeyFlags, HashAlgorithm, SymmetricKeyAlgorithm, CompressionAlgorithm
-from Crypto.PublicKey import RSA
-from Crypto import Random
+from pgpy.constants import PubKeyAlgorithm, KeyFlags, HashAlgorithm,\
+                           SymmetricKeyAlgorithm, CompressionAlgorithm
 
-#creates new PGPkeys (privatekey, publickey)
+# creates new RSA PGPkeys (privatekey, publickey) 
+# Size of RSA key is 4096
+# @param: name string
+# @param: email: string
+# @return: (sk PGPkey, pk PGPkey.pubkey)
 def create_pgpkey(name, email):
     key = pgpy.PGPKey.new(PubKeyAlgorithm.RSAEncryptOrSign, 4096)
     uid = pgpy.PGPUID.new(name, email=email)
     key.add_uid(uid, usage={KeyFlags.Sign, KeyFlags.EncryptCommunications, KeyFlags.EncryptStorage},
-            hashes=[HashAlgorithm.SHA256, HashAlgorithm.SHA384, HashAlgorithm.SHA512, HashAlgorithm.SHA224],
+            hashes=[HashAlgorithm.SHA512, HashAlgorithm.SHA384, HashAlgorithm.SHA256, HashAlgorithm.SHA224],
             ciphers=[SymmetricKeyAlgorithm.AES256, SymmetricKeyAlgorithm.AES192, SymmetricKeyAlgorithm.AES128],
             compression=CompressionAlgorithm.ZIP)
     return key, key.pubkey
 
-#returns pgpy.PGPkey
+# Import key from file
+# @param: filename string
+# @return: k PGPkey
 def read_key_from_file(filename):
     return pgpy.PGPKey.from_file(filename)
 
-#returns pgpy.PGPMessage from a string
-def create_pgp_message(message):
-    return pgpy.PGPMessage.new(message, encoding="UTF8")
+# Create PGPMessage from string
+# @param: m string
+# @return: pgp_m PGPMessage
+def create_pgp_message(m):
+    return pgpy.PGPMessage.new(m, encoding="UTF8")
 
-#returns pgpy.PGPMessage from a file
-def create_pgp_message_from_file(filepath):
-    return pgpy.PGPMessage.new(filepath, file=True)
+# Create PGPMessage from file
+# @param: filename string
+# @return: pgp_m PGPMessage
+def create_pgp_message_from_file(filename):
+    return pgpy.PGPMessage.new(filename, file=True)
 
-#uses the public key to encrypt the message (pgpy.PGPMessage, pgpy.PGPKey -> pgpy.PGPMessage)
-def encrypt(pgp_msg, pk):
-    return pk.encrypt(pgp_msg)
+# Encrypt string
+# @param: m string
+# @param: pk PGPkey.pubkey
+# @return: pgp_m PGPMessage
+def encrypt(m, pk):
+    return str(pk.encrypt(create_pgp_message(m)))
 
-#uses the private key to decrypt the message (pgpy.PGPMessage, pgpy.PGPKey -> pgpy.PGPMessage)
-def decrypt(pgp_msg, sk):
-    return sk.decrypt(pgp_msg)
+# Encrypt file
+# @param: filename string
+# @param: pk PGPkey.pubkey
+# @return: pgp_m string
+def encrypt_file(filename, pk):
+    return str(pk.encrypt(create_pgp_message_from_file(filename)))
 
-#uses the masterkey (trusted 3rd party) to verify public key (takes paths as input)
+# Decrypt string
+# @param: c string
+# @param: sk PGPkey
+# @return: m string
+def decrypt(c, sk):
+    pgpy.PGPMessage.from_blob(c)
+    return sk.decrypt(pgpy.PGPMessage.from_blob(c)).message
+
+# Decrypt file
+# @param: filename string
+# @param: sk PGPkey
+# @return: m string
+def decrypt_file(filename, sk):
+    return decrypt(open(filename, 'rb').read(), sk)
+
+# uses the masterkey (trusted 3rd party) to verify public key
+# Calculate MD5 hash
+# @param: masterkeyfile string
+# @param: pkfile string
+# @return: verify boolean
 def verify(masterkeyfile, pkfile):
     pk = read_key_from_file(pkfile)
     master = read_key_from_file(masterkeyfile)
     return master.verify(pk)
 
-def md5_hash(message):
-    dig = hashlib.md5()
-    dig.update(message)
-    return dig.digest()
+# Calculate MD5 hash
+# @param: m string
+# @return: h string
+def md5_hash(m):
+    digest = hashlib.md5()
+    for i in range(0, len(m), 512):
+        digest.update(m[i:i+512])
+    return digest.digest()
 
-def verify_md5_hash(message, hash):
-    dig = hashlib.md5()
-    dig.update(message)
-    return dig.digest() == hash
+# Verify MD5 hash
+# @param: m string
+# @param: h string
+# @return: b boolean
+def verify_md5_hash(m, h):
+    digest = hashlib.md5()
+    for i in range(0, len(m), 512):
+        digest.update(m[i:i+512])
+    return digest.digest() == h
 
-#MD5 test
-#hash = md5_hash("Hey")
-#print(verify_md5_hash("Hey", hash))
+# Unit testing function
+# TODO test file encryption
+def unitTest():
+    # Hash test
+    assert (verify_md5_hash(b"Hello World!", md5_hash(b"Hello World!")))
+    
+    # Encryption test
+    sk, pk = create_pgpkey("Bruno Produit", "bruno@produit.name")
+    assert ("Hello World!" == decrypt(encrypt("Hello World!", pk), sk))
 
-
-
-secretkey, publickey = create_pgpkey("anne kala", "kala_ann@ttu.ee")
-
-msg = create_pgp_message(b"Hey")
-
-print(msg)
-
-enc = encrypt(msg, publickey)
-print(type(enc))
-
-dec = decrypt(enc, secretkey)
-print(dec)
+unitTest()
